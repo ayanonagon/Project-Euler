441693a48a3eb6817e591635e150371852e48311
Ayaka Nonaka <ayanonagon@gmail.com>
2013-12-24 15:14:19 -0800

    Uses select instead of find_all.

--- problem1.rb
+++ problem1.rb
@@ -1,2 +1,2 @@
-sol = (1..999).find_all{|n| n % 3 == 0 or n % 5 == 0}.inject(0){|sum, n| sum + n} 
+sol = (1..999).select{|n| n % 3 == 0 or n % 5 == 0}.inject(0){|sum, n| sum + n}
 puts "The answer to problem 1 is: #{sol}"
